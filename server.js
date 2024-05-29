d02be95b7fd4febb2210e3596966c0b80bfd0b88
Rar Zar <yarzarminkhant2003@gmail.com>
2024-05-29 15:14:49 +0630

    env setup

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const mongoose = require("mongoose");
 const router = require("./router/web.js");
+require("dotenv").config();
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
@@ -11,13 +12,13 @@ app.set("layout", "layouts/master");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true })); //for post request data middleware
 
-const mongoUrl =
-  "mongodb+srv://yarzarminkhant2003:LPT1500%40love@cluster0.6253mxi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
 mongoose
-  .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
-    app.listen(8000, () => {
+    app.listen(process.env.PORT, () => {
       console.log("Server is running on port 8000 and connected to MongoDB");
     });
   })
